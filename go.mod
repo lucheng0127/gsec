8e1d03199e1118462a8e179a4be7903a6897f607
ShawnLu <lucheng0127@outlook.com>
2023-04-14 23:31:43 +0800

    Use test suit test auth function

--- go.mod
+++ go.mod
@@ -7,4 +7,11 @@ replace github.com/lucheng0127/gsec => ./
 require (
 	bou.ke/monkey v1.0.2
 	github.com/golang/mock v1.6.0
+	github.com/stretchr/testify v1.8.2
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ pkg/server/auth_test.go
@@ -0,0 +1,176 @@
+package server
+
+import (
+	"errors"
+	"reflect"
+	"testing"
+
+	"bou.ke/monkey"
+	"github.com/golang/mock/gomock"
+	"github.com/lucheng0127/gsec/mocks"
+	"github.com/lucheng0127/gsec/mocks/mock_cipher"
+	"github.com/lucheng0127/gsec/pkg/auth"
+	"github.com/lucheng0127/gsec/pkg/cipher"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
+)
+
+type AuthTestSuite struct {
+	suite.Suite
+	userbook map[string]string
+}
+
+func (s *AuthTestSuite) SetupTest() {
+	s.userbook = make(map[string]string)
+	s.userbook["lucheng"] = "0123456789abcdef"
+}
+
+func (s *AuthTestSuite) TestHandle() {
+	ctrl := gomock.NewController(s.T())
+	defer ctrl.Finish()
+
+	mock_auth := mocks.NewMockAuth(ctrl)
+	monkey.Patch(auth.NewUserAuth, func(username, psk string) auth.Auth {
+		return mock_auth
+	})
+
+	type args struct {
+		username string
+		autoData []byte
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "user not exist",
+			args: args{
+				username: "not exist user",
+				autoData: make([]byte, 0),
+			},
+			wantErr: true,
+		},
+		{
+			name: "validate err",
+			args: args{
+				username: "lucheng",
+				autoData: make([]byte, 0),
+			},
+			wantErr: true,
+		},
+		{
+			name: "validate failed",
+			args: args{
+				username: "lucheng",
+				autoData: make([]byte, 0),
+			},
+			wantErr: true,
+		},
+		{
+			name: "ok",
+			args: args{
+				username: "lucheng",
+				autoData: make([]byte, 0),
+			},
+			wantErr: false,
+		},
+	}
+
+	for _, tt := range tests {
+		if tt.name == "validate err" {
+			mock_auth.EXPECT().Validate(tt.args.autoData).Return(false, errors.New("vaildate err"))
+		}
+		if tt.name == "validate failed" {
+			mock_auth.EXPECT().Validate(tt.args.autoData).Return(false, nil)
+		}
+		if tt.name == "ok" {
+			mock_auth.EXPECT().Validate(tt.args.autoData).Return(true, nil)
+		}
+
+		err := handle_auth(s.userbook, tt.args.username, tt.args.autoData)
+		if tt.wantErr {
+			assert.NotNil(s.T(), err)
+		} else {
+			assert.Nil(s.T(), err)
+		}
+	}
+}
+
+func TestAuthTestSuit(t *testing.T) {
+	suite.Run(t, new(AuthTestSuite))
+}
+
+func Test_generate_auth_payload(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mock_cipher := mock_cipher.NewMockCipher(ctrl)
+
+	type args struct {
+		username string
+		psk      string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    []byte
+		wantErr bool
+	}{
+		{
+			name: "cipher err",
+			args: args{
+				username: "lucheng",
+				psk:      "0123456789abcdef",
+			},
+			want:    make([]byte, 0),
+			wantErr: true,
+		},
+		{
+			name: "encrypt err",
+			args: args{
+				username: "lucheng",
+				psk:      "0123456789abcdef",
+			},
+			want:    make([]byte, 0),
+			wantErr: true,
+		},
+		{
+			name: "ok",
+			args: args{
+				username: "lucheng",
+				psk:      "0123456789abcdef",
+			},
+			want:    []byte("ok"),
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if tt.name == "cipher err" {
+				monkey.Patch(cipher.NewAESCipher, func(key string) (cipher.Cipher, error) {
+					return nil, errors.New("cipher err")
+				})
+			} else {
+				monkey.Patch(cipher.NewAESCipher, func(key string) (cipher.Cipher, error) {
+					return mock_cipher, nil
+				})
+				if tt.name == "encrypt err" {
+					mock_cipher.EXPECT().Encrypt([]byte(tt.args.username)).Return(make([]byte, 0), errors.New("encrypt err"))
+				}
+				if tt.name == "ok" {
+					mock_cipher.EXPECT().Encrypt([]byte(tt.args.username)).Return([]byte("ok"), nil)
+				}
+			}
+
+			got, err := generate_auth_payload(tt.args.username, tt.args.psk)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("generate_auth_payload() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("generate_auth_payload() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
